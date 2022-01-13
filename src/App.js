4e10a7055ecf906ecec50394f43ee583ce6ec984
Steph Heins <stephanie_heins@yahoo.com>
2022-01-13 01:53:08 -0800

    Created state and object array placeholder -- creating items for selecting now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [appState, changeState] = useState({
+    activeObject: null,
+    objects: [{id: 1}, {id: 2}, {id: 3}]
+  });
+
   return (
     <div className="App">
       <h1>
         Test
       </h1>
+
+      { appState.objects.map((element, index) => (
+        <div>
+        </div>
+      )) }
     </div>
   );
 }
